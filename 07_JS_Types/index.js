a645afd4ca65ce23419c4b612f704414e2f4272a
jordan <jharrison1@qa.com>
2023-09-01 10:15:48 +0100

    operators

--- 07_JS_Types/index.js
+++ 07_JS_Types/index.js
@@ -1,5 +1,12 @@
+"use strict";
+
 console.log("JS FILE:");
 
 const sum = 2 + 2;
 
 console.log("SUM:", sum);
+
+{
+    const sum = 44;
+    console.log("SUM:", sum);
+}
---
+++ 08_JS_Operators/index.js
@@ -0,0 +1,67 @@
+"use strict";
+
+console.log("6 + 6 =", 6 + 6);
+console.log("6 - 6 =", 6 - 6);
+console.log("6 * 6 =", 6 * 6);
+console.log("6 / 6 =", 6 / 6);
+console.log("6 ** 6 =", 6 ** 6);
+console.log("10 % 6 =", 10 % 6);
+
+// concatenation
+console.log("27" + "27");
+
+console.log("Jordan" + " "  + "Harrison");
+
+// type coercion
+console.log("27" * "27");
+
+console.log("Jordan" * "Harrison");
+
+console.log("NaN:", typeof NaN);
+
+// Boolean
+console.log(true && false);
+
+console.log(true || false);
+
+console.log(2 * 2 === 4 && 5 > 3);
+
+console.log(2 * 2 === 4 && 5 > 6);
+
+console.log(2 * 2 === 4 || 5 > 6);
+
+console.log(!true);
+console.log(!false);
+
+let name = "Steve";
+
+let me = `Hello, my name is ${name}`;
+
+console.log("Me:", me);
+
+let num = 12;
+
+console.log("Num:", num);
+
+console.log("Num++:", num++);
+
+console.log("Num:", num);
+
+console.log("++Num:", ++num);
+
+console.log("Num:", num);
+
+console.log("Num--:", num--);
+
+console.log("Num:", num);
+
+console.log("--Num:", --num);
+
+console.log("Num:", num);
+
+console.log("Num += 12:", num += 12);
+console.log("Num -= 12:", num -= 12);
+console.log("Num *= 12:", num *= 12);
+console.log("Num /= 12:", num /= 12);
+console.log("Num %= 7:", num %= 7);
+console.log("Num **= 7:", num **= 7);
